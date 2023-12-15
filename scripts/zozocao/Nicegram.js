e3a9ca32d4bbbfedb437c10f5beb47c3465df60d
zozocao <127742748+zozocao@users.noreply.github.com>
2023-12-15 10:17:01 +0700

    Update Nicegram.js

--- scripts/zozocao/Nicegram.js
+++ scripts/zozocao/Nicegram.js
@@ -2,7 +2,8 @@ const obj = {
   "data": {
     "subscription": true,
     "store_subscription": true,
-    "lifetime_subscription": true
+    "lifetime_subscription": true,
+    "premiumAccess": true
   }
 };
 $done({status:200,headers:{'Content-Type':'application/json'},body:JSON.stringify(obj)});
