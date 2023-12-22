b282599e367039b210ebebb37019d4f9b4bde7e6
zozocao <127742748+zozocao@users.noreply.github.com>
2023-12-22 19:55:10 +0700

    Update Nicegram.js

--- scripts/zozocao/Nicegram.js
+++ scripts/zozocao/Nicegram.js
@@ -1,9 +1,25 @@
-const obj = {
-  "data": {
+
+const isQX = typeof $task !== "undefined";
+var chxm1023 = JSON.parse($response.body);
+const subscriptionTest = /https:\/\/nicegram\.cloud\/api\/v\d\/user\/info/;
+const premiumTest = /https:\/\/restore-access\.indream\.app\/restoreAccess/;
+
+if (subscriptionTest.test(url)) {
+  chxm1023.data.user = {
+    ...chxm1023.data.user,
     "subscription": true,
     "store_subscription": true,
     "lifetime_subscription": true,
     "premiumAccess": true
-  }
-};
-$done({status:200,headers:{'Content-Type':'application/json'},body:JSON.stringify(obj)});
+  };
+}
+
+if (premiumTest.test(url)) {
+  chxm1023["data"] = {"premiumAccess": true};
+}
+
+function finalizeResponse(content) {
+  return { status: isQX ? "HTTP/1.1 200 OK" : 200, headers: $response.headers, body: JSON.stringify(content) };
+}
+
+$done(isQX ? finalizeResponse(chxm1023) : chxm1023);
