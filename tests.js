d5a4932424387a164e3a820c590e530b0b5be860
zozocao <127742748+zozocao@users.noreply.github.com>
2024-01-18 13:45:14 +0700

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,9 +1,8 @@
 const obj = {
   "data": {
-    "premiumAccess": true;
-    "lifetime_subscription": true;
-    "store_subscription": true;
-    "subscription": true;
+    "lifetime_subscription": true
+    "store_subscription": true
+    "subscription": true
   }
 };
 $done({status:200,headers:{'Content-Type':'application/json'},body:JSON.stringify(obj)});
