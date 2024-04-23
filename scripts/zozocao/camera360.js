4c2a7808875fb07592556bc36c6d976eb49472cb
zozocao <127742748+zozocao@users.noreply.github.com>
2024-04-23 21:28:35 +0700

    Update camera360.js

--- scripts/zozocao/camera360.js
+++ scripts/zozocao/camera360.js
@@ -1,17 +1,38 @@
-let obj= {
-  "status": 200,
-  "data": {
-    "sandbox": 1,
-    "purchaseTime": 1571682188,
-    "giftVip": 0,
-    "productId": "VIP_yearly_29.99",
-    "appleVip": 1,
-    "expireTime": 4096532852,
-    "operationVip": 1,
-    "errorCode": 0
-  },
-  "message": "ok",
-  "exetime": "1572056389212-1572056391362",
-  "serverTime": 1572056391.3622
-};
-$done({body: JSON.stringify(obj)});
+var chxm1023 = JSON.parse($response.body);
+
+const vip1 = '/api/order/purchase';
+const vip2 = '/api/iap/check-receipt';
+const ad = '/operational-positions';
+
+if ($request.url.indexOf(vip1) != -1){
+  chxm1023["data"] = {
+    "originalTransactionId" : "490001464780901",
+    "errorCode" : 0,
+    "purchaseTime" : 1662685749,
+    "isTrialPeriod" : 1,
+    "expireTime" : 4092599349,
+    "sandbox" : 0
+  };
+}
+
+if ($request.url.indexOf(vip2) != -1){
+  chxm1023["data"] = {
+    "sandbox" : 0,
+    "purchaseTime" : 1662685749,
+    "isTrialPeriod" : 1,
+    "originalTransactionId" : "490001464780901",
+    "appleExpireTime" : 4092599349,
+    "productId" : "vip_yearly_3daysfree",
+    "appleVip" : 1,
+    "expireTime" : 4092599349,
+    "giftVip" : 1,
+    "operationVip" : 1,
+    "errorCode" : 0
+  };
+}
+
+if ($request.url.indexOf(ad) != -1){
+  chxm1023.Boot = [];
+}
+
+$done({body : JSON.stringify(chxm1023)});
