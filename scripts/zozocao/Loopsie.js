b477d55f9edda21156dbe70f2904cc12c2ba3f7d
zozocao <127742748+zozocao@users.noreply.github.com>
2024-05-15 20:00:58 +0700

    Create Loopsie.js

---
+++ scripts/zozocao/Loopsie.js
@@ -0,0 +1,28 @@
+const py996 = {};
+const py997 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+
+const name = "pro-iOS";
+const appid = "com.gamelounge.loopsie.ios.one_time";
+
+  
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+
+  py996.headers = $request.headers;
+} else if (py997 && py997.subscriber) {
+  data = {
+    "expires_date": "9999-09-09T09:09:09Z",
+    "original_purchase_date": "2024-02-23T02:33:33Z",
+    "purchase_date": "2024-02-23T02:33:33Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+
+  py997.subscriber.subscriptions[(appid)] = data
+  py997.subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
+  py997.subscriber.entitlements[(name)].product_identifier = (appid);
+  py996.body = JSON.stringify(py997);
+}
+
+$done(py996);
